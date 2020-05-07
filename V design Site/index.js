03db7e113a842a3bfd2d56c85940214273e2df1a
Weiwei Huang <vivihwong@gmail.com>
2020-05-07 15:57:29 -0400

    Initial package
    start kit

---
+++ V design Site/index.js
@@ -0,0 +1,21 @@
+$(function () {
+    setNavigation();
+});
+
+function setNavigation() {
+    var path = window.location.pathname;
+    path = path.replace(/\/$/, "");
+    path = decodeURIComponent(path);
+
+    $(".nav a").each(function () {
+        var href = $(this).attr('href');
+        if (path.substring(0, href.length) === href) {
+            $(this).closest('li').addClass('active');
+        }
+    });
+}
+
+
+$(document).ready(function() {
+
+});
